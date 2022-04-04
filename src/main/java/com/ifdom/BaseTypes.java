c77d7de4843bd6109ce86d4545233040cec1cc2f
ifredom <ifredomvip@gmail.com>
2022-04-04 20:19:05 +0800

    date

--- src/main/java/com/ifdom/BaseTypes.java
+++ src/main/java/com/ifdom/BaseTypes.java
@@ -1,35 +1,67 @@
 package com.ifdom;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * 实现了 Serializable 串行化，那么可以网络传输
  * String  深入 https://www.bilibili.com/video/BV1fh411y7R8?p=468&spm_id_from=pageDriver
  * StringBuffer 存在堆中，非常量池
- *
+ * <p>
  * String 效率差，复用率高
  * StringBuffer 线程安全，增删 ，效率高
  * StringBuilder   线程不安全，效率最高
- *
+ * <p>
  * 单线程，大量修改，使用 StringBuilder
  * 多线程，大量修改，使用 StringBuffer
  * 配置信息，String
  */
 public class BaseTypes {
     public static void main(String[] args) {
-        testStringBuffer();
+        String sortString = reverse("abcdefgh", 1, 7);
+
+        System.out.println(sortString);
+
+
+    }
+
+    private static String reverse(String str, int start, int end) {
+        char[] chars = str.toCharArray();
+        char temp = ' ';
+        for (int i = start, j = end; i < j; i++, j--) {
+            temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+        return new String(chars);
     }
 
 
+    /**
+     * LocalDate, LocalTime, LocalDateTime
+     */
+    private static void testDate() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDateTime now = LocalDateTime.now();
+        String str = now.format(dateTimeFormatter);
+        System.out.println(str);
+
+        LocalDate now1 = LocalDate.now();
+        String str1 = now1.format(dateTimeFormatter1);
+        System.out.println(str1);
+
+    }
 
 
-    private static  void testStringBuffer(){
-        String str =null;
+    private static void testStringBuffer() {
+        String str = null;
 
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append(str);
-//
 //        StringBuffer stringBuffer2 = new StringBuffer(str);
-
-
         StringBuilder stringBuilder = new StringBuilder();
 
     }
--- src/main/java/com/ifdom/demo/DemoApplication.java
+++ src/main/java/com/ifdom/demo/DemoApplication.java
@@ -4,6 +4,12 @@ import com.ifdom.designmode.SingleInstance;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 @Slf4j
 @SpringBootApplication
 public class DemoApplication {
@@ -11,24 +17,79 @@ public class DemoApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(DemoApplication.class, args);
 
-        SingleInstance test = SingleInstance.getInstance("小红");
-        SingleInstance test2 = SingleInstance.getInstance("小绿");
+//        testCompare();
+        String  test = "1234";
 
-        System.out.println(test);
-        System.out.println(test2);
-        System.out.println(test == test2);
+        byte[] bytes =  {'1','2','3','4'};
+
+        System.out.println(bytes.toString());
+        System.out.println(test.equals(bytes.toString()) );
+
+        BigInteger bigInteger = new BigInteger("123");
+
     }
+
+    public static void testCompare() {
+        Book[] books = new Book[4];
+        books[0] = new Book("hh", 100);
+        books[1] = new Book("xx", 300);
+        books[2] = new Book("aa", 900);
+        books[3] = new Book("tt", 200);
+
+        for (Book item : books) {
+            System.out.println(item);
+        }
+
+        test2(books);
+
+        for (Book item : books) {
+            System.out.println(item);
+        }
+        System.out.println("\n==============");
+        Book[] latest = Arrays.copyOf(books,2);
+        for (Book item : latest) {
+            System.out.println(item);
+        }
+
+    }
+
+    public static void test2(Book[] books) {
+        Arrays.sort(books, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                int result = 0;
+                if (o1.price - o2.price > 0) {
+                    result = 1;
+                } else if (o1.price - o2.price < 0) {
+                    result = -1;
+                } else {
+                    result = 0;
+                }
+                return result;
+            }
+        });
+    }
+
 }
 
-class Test {
+class Book {
+    public String name;
+    public int price;
 
-    public Test() {
-        System.out.println("Test constructor");
+    public Book(String name, int price) {
+        this.name = name;
+        this.price = price;
     }
 
-    {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+    public Book() {
+        System.out.println("TestArrays constructor");
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
