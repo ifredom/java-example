c69edbc72160e85d52e69d2c8221119aec3821c0
ifredom <1950735817@qq.com>
2022-04-14 01:03:21 +0800

    feat: 虚拟mvc http

---
+++ common-srpingboot/src/test/java/com/springboot/WebTest.java
@@ -0,0 +1,23 @@
+package com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// 开启虚拟MVC调用
+@AutoConfigureMockMvc
+public class WebTest {
+
+    @Test
+    void testWeb(@Autowired MockMvc mvc) throws Exception {
+
+        RequestBuilder builder= MockMvcRequestBuilders.get("/students");
+        mvc.perform(builder);
+    }
+}
